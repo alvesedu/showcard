4d3a478681a9a43c1940de29b63bd8597255ee5f
alvesedu <edu.rib.alves@gmail.com>
2020-04-20 19:47:55 -0300

    commit inicial

---
+++ App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {Card} from 'react-native-shadow-cards';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Card style={{padding: 10, margin: 10}}>
+          <Text>Plantão - Unidade</Text>
+          <Text>Data</Text>
+          <Text>Unidade</Text>
+        </Card>
+
+        <Card style={{padding: 10, margin: 10}}>
+          <Text>Plantão - Unidade</Text>
+          <Text>Data</Text>
+          <Text>Unidade</Text>
+        </Card>
+
+        <Card style={{padding: 10, margin: 10}}>
+          <Text>Plantão - Unidade</Text>
+          <Text>Data</Text>
+          <Text>Unidade</Text>
+        </Card>
+
+        <Card style={{padding: 10, margin: 10}}>
+          <Text>Plantão - Unidade</Text>
+          <Text>Data</Text>
+          <Text>Unidade</Text>
+        </Card>
+
+        <Card style={{padding: 10, margin: 10}}>
+          <Text>Plantão - Unidade</Text>
+          <Text>Data</Text>
+          <Text>Unidade</Text>
+        </Card>
+        
+     
+      
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
